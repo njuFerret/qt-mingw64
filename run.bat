a2464d1372c06375e240cf7deb105b176c323a39
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-09-18 12:43:55 +0530

    Update build script

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+set BUILD_START_DIR=%CD%
+set RELEASE_BUILD_DIR=%BUILD_START_DIR%\qt_release_mingw64
+
 curl -L -o 7zr.exe https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/7zr.exe
 curl -L -o cmake-3.27.4-windows-x86_64.zip https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/cmake-3.27.4-windows-x86_64.zip
 curl -L -o libclang-release_140-based-windows-mingw_64.7z https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/libclang-release_140-based-windows-mingw_64.7z
@@ -7,9 +10,12 @@ curl -L -o openssl-mingw-bin.tar.gz https://github.com/FetheredSerpent/qt-mingw6
 curl -L -o qt-everywhere-src-6.5.2.tar.xz https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/qt-everywhere-src-6.5.2.tar.xz
 curl -L -o strawberry-perl-5.32.1.1-64bit.zip https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/strawberry-perl-5.32.1.1-64bit.zip
 curl -L -o 7z2301-extra.7z https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/7z2301-extra.7z
+curl -L -o ffmpeg-6.0-full_build-shared.7z https://github.com/FetheredSerpent/qt-mingw64/releases/download/dependencies/ffmpeg-6.0-full_build-shared.7z
 MKDIR strawberry
 MKDIR ninja
 
+echo Extracting ffmpeg
+7zr x ffmpeg-6.0-full_build-shared.7z
 echo Extracting cmake
 tar -xf cmake-3.27.4-windows-x86_64.zip
 echo Extracting libclang
@@ -32,6 +38,7 @@ rem tar -xf qt-everywhere-src-6.5.2.tar > NUL 2>1
 
 echo Clearing archives
 RMDIR /S /Q 7z
+DEL ffmpeg-6.0-full_build-shared.7z
 DEL 7z2301-extra.7z
 DEL cmake-3.27.4-windows-x86_64.zip
 DEL libclang-release_140-based-windows-mingw_64.7z
@@ -42,18 +49,22 @@ DEL strawberry-perl-5.32.1.1-64bit.zip
 DEL qt-everywhere-src-6.5.2.tar.xz
 DEL qt-everywhere-src-6.5.2.tar
 
+SET CMAKE_PREFIX_PATH=%CD%\ffmpeg-6.0-full_build-shared;%RELEASE_BUILD_DIR%
 SET OPENSSL_ROOT_DIR=%CD%\openssl-mingw-bin
 SET LLVM_INSTALL_DIR=%CD%\libclang
-SET PATH=%CD%\ninja;%CD%\mingw64\bin;%CD%\cmake-3.27.4-windows-x86_64\bin;%CD%\strawberry\perl\bin;%CD%\qt-everywhere-src-6.5.2;%PATH%
+SET PATH=%CD%\ninja;%CD%\mingw64\bin;%CD%\cmake-3.27.4-windows-x86_64\bin;%CD%\strawberry\perl\bin;%RELEASE_BUILD_DIR%\bin;%CD%\qt-everywhere-src-6.5.2;%PATH%
 
 echo Starting build
-CD qt-everywhere-src-6.5.2
-MKDIR out 
-CD out
-call ..\configure.bat -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -openssl-runtime -opengl dynamic -prefix %CD%\qt_release_mingw64 -release -opensource -nomake examples -nomake tests -skip qtwebengine
-rem mingw32-make
-cmake --build . --target help
-rem cmake --build . --parallel
-rem cmake --install .
+set FULL_SOURCE_DIRECTORY=%CD%\qt-everywhere-src-6.5.2
+call scripts\build.bat
+REM CD qt-everywhere-src-6.5.2
+REM MKDIR out 
+REM CD out
+REM call ..\configure.bat -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -openssl-runtime -opengl dynamic -prefix %CD%\qt_release_mingw64 -release -opensource -nomake examples -nomake tests -skip qtwebengine
+REM mingw32-make
+REM cmake --build . --target help
+REM cmake --build . --parallel
+REM cmake --install .
 
-tar -czf ..\..\qt_release_mingw64.tar.gz %CD%\qt_release_mingw64
+CD %BUILD_START_DIR%
+tar -czf qt_release_mingw64.tar.gz qt_release_mingw64
