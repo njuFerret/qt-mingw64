c99aef63a474f9bce588d5c1f411e15e36c3c8e3
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-09-18 13:11:37 +0530

    Update level build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,7 @@
-call level_0_build.bat
-call level_1_build.bat
-call level_2_build.bat
-call level_3_build.bat
-call level_4_build.bat
-call level_5_build.bat
-call level_6_build.bat
+call %BUILD_START_DIR%\scripts\level_0_build.bat
+call %BUILD_START_DIR%\scripts\level_1_build.bat
+call %BUILD_START_DIR%\scripts\level_2_build.bat
+call %BUILD_START_DIR%\scripts\level_3_build.bat
+call %BUILD_START_DIR%\scripts\level_4_build.bat
+call %BUILD_START_DIR%\scripts\level_5_build.bat
+call %BUILD_START_DIR%\scripts\level_6_build.bat
