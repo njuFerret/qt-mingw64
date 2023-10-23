9fa3d5539fec6f65ffeb61a5be007afe543ff637
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-10-23 13:22:13 +0530

    Update build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -18,7 +18,7 @@ CD %FULL_SOURCE_DIRECTORY%\qtbase
 MKDIR out && CD out
 call ..\configure.bat -qt-zlib -qt-libjpeg -qt-libpng ^
     -qt-freetype -qt-pcre -qt-harfbuzz -openssl-runtime ^
-    -opengl dynamic -prefix %BUILD_OUTPUT_DIR% %BUILD_FLAG% ^
+    -opengl dynamic -prefix %BUILD_OUTPUT_DIR% -release ^
     -opensource -nomake examples -nomake tests
 cmake --build . --parallel
 cmake --install .
